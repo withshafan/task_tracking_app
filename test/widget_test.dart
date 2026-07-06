70bd77ab1c172d4dc5c4451332add2e068cadeb6
MUHAMMAD SHAFAN RIAZ <malikshafan80@gmail.com>
2026-07-06 15:59:10 +0500

    Build: Fix package import in widget_test.dart after app rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:task_tracking_app/main.dart';
+import 'package:taskflow/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
